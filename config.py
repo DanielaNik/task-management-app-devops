813170ed06ef7ef92a14a5d476becd3a0866d9b8
danielaNik <daniela.nikolovska@hotmail.com>
2023-09-13 23:49:08 +0200

    kubernetes manifests

--- config.py
+++ config.py
@@ -1,18 +1,8 @@
-import os
-
 DB_CONFIG = {
-    'dbname' : os.environ.get('POSTGRES_DB'),
-    'user' : os.environ.get('POSTGRES_USER'),
-    'password' : os.environ.get('POSTGRES_PASSWORD'),
-    'host' : os.environ.get('POSTGRES_HOST'),
-    'port' : os.environ.get('POSTGRES_PORT')
+    'dbname' : 'tasks',
+    'user' : 'postgres',
+    'password' : 'admin',
+    'host' : 'db',
+    'port' : 5432
 }
 
-if DB_CONFIG['dbname'] is None:
-    raise RuntimeError("Environment variable POSTGRES_DB is missing")
-
-if DB_CONFIG['user'] is None:
-    raise RuntimeError("Environment variable POSTGRES_USER is missing")
-
-if DB_CONFIG['password'] is None:
-    raise RuntimeError("Environment variable POSTGRES_PASSWORD is missing")
